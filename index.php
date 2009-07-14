a80d73a4c36e4ccfb3eea8cc81741ae080d901c8
Christian Heilmann <chris.heilmann@gmail.com>
2009-07-14 16:44:46 +0100

    layout

--- index.php
+++ index.php
@@ -8,9 +8,15 @@
   <link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/base/base.css" type="text/css">
 </head>
 <body>
-<div id="doc" class="yui-t7">
+<div id="doc" class="yui-t1">
   <div id="hd" role="banner"><h1>RSS 2 Map - turn an RSS feed into a map :)</h1></div>
   <div id="bd" role="main">
+    <div id="yui-main">
+      <div class="yui-b first">
+      <?php include('rss2map.php');?>
+      </div>
+    </div>
+    <div class="yui-b">
     <ul>
       <li><a href="index.php?feed=http://www.telegraph.co.uk/travel/rss">Telegraph Travel</a></li>
       <li><a href="index.php?feed=http://www.telegraph.co.uk/news/rss">Telegraph News</a></li>
@@ -18,10 +24,10 @@
       <li><a href="index.php?feed=http://rss.news.yahoo.com/rss/world">Yahoo World News</a></li>
       <li><a href="index.php?feed=http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml">BBC World News</a></li>
     </ul>
-    <?php include('rss2map.php');?>
+    </div>
   </div>
-  <div id="ft" role="contentinfo"><p></p></div>
-</div>
+  <div id="ft" role="contentinfo"><p>RSS2Map written by <a href="http://icant.co.uk">Christian Heilmann</a> using <a href="http://developer.yahoo.com/yui">YUI</a> and <a href="http://developer.yahoo.com/geo/placemaker">Placemaker</a></p></div>
+  </div>
 </body>
 </html>
 
