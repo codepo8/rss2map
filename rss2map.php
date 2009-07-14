ac59d9817a722133e550d6054fbd775a2a870b64
Christian Heilmann <chris.heilmann@gmail.com>
2009-07-14 16:31:58 +0100

    added check to enclose html when used in an iframe

--- rss2map.php
+++ rss2map.php
@@ -1,10 +1,22 @@
-<?php if(isset($_GET['feed'])){?>
+<?php 
+if(preg_match('/rss2map.php$/',$_SERVER["PHP_SELF"])){?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">  
+  <title>RSS 2 Map result</title>
+  <link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
+  <link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/base/base.css" type="text/css">
+</head>
+<body>
+<?php }
+if(isset($_GET['feed'])){?>
 <?php  
   $url = $_GET['feed'];
   $key = 'PbXUT7HV34Fq2KhMd68qS.CRZY9RWjW_dEQLgINMwG.eNxu2hf84BTkvHNttEg4-';
 
   // ^ get your own! 
-
   $apiendpoint = 'http://wherein.yahooapis.com/v1/document';
   $inputType = 'text/rss';
   $outputType = 'rss';
@@ -51,4 +63,9 @@
       var YMAPPID = '<?php echo $key;?>';
       rss2map(<?php echo $output;?>);
   </script>
+<?php } 
+if(preg_match('/rss2map.php$/',$_SERVER["PHP_SELF"])){?>
+</body>
+</html>
 <?php } ?>
+
